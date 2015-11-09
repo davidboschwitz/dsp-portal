8ed72be1fac5079ce19aff5acb8ee9031ec9e7ac
David Boschwitz <hihid27@aim.com>
2015-11-08 23:37:28 -0600

    created file

---
+++ getusers.php
@@ -0,0 +1,43 @@
+<?php
+
+$page['auth'] = 1;
+include 'include/functions.inc';
+include 'include/mysql.inc';
+$search = filter_input(INPUT_GET, 'term', FILTER_SANITIZE_SPECIAL_CHARS);
+if (isset($_GET['multiple'])) {
+    $start = 0;
+//echo $search;
+    while (strpos($search, ",") > 0) {
+        $start += ($a = strpos($search, ",") + 1);
+        $search = substr($search, $a);
+    }
+    $search = trim($search);
+    $query = sprintf("SELECT *  FROM `dsp`.`dsp_users` WHERE `user` LIKE '%s%%' OR `first_name` LIKE '%s%%' OR `last_name` LIKE '%s%%'", mysql_real_escape_string($search), mysql_real_escape_string($search), mysql_real_escape_string($search));
+//echo $query;
+    $result = mysql_query($query) or die('Invalid query: ' . mysql_error());
+    $i = 0;
+    echo "[";
+    while ($row[$i] = mysql_fetch_assoc($result)) {
+        if ($i > 0)
+            echo ", ";
+        echo "{ \"label\": \"" . $row[$i]['last_name'] . ", " . $row[$i]['first_name'] . " (" . $row[$i]['user'] . ")\", \"value\": \"" . substr($_GET['term'], 0, $start) . " " . $row[$i]['user'] . ", \"}";
+
+        $i++;
+    }
+    echo "]";
+}else {
+    $search = trim($search);
+    $query = sprintf("SELECT *  FROM `dsp`.`dsp_users` WHERE `user` LIKE '%s%%' OR `first_name` LIKE '%s%%' OR `last_name` LIKE '%s%%'", mysql_real_escape_string($search), mysql_real_escape_string($search), mysql_real_escape_string($search));
+//echo $query;
+    $result = mysql_query($query) or die('Invalid query: ' . mysql_error());
+    $i = 0;
+    echo "[";
+    while ($row[$i] = mysql_fetch_assoc($result)) {
+        if ($i > 0)
+            echo ", ";
+        echo "{ \"label\": \"" . $row[$i]['last_name'] . ", " . $row[$i]['first_name'] . " (" . $row[$i]['user'] . ")\", \"value\": \"" . $row[$i]['user'] . "\"}";
+
+        $i++;
+    }
+    echo "]";
+}

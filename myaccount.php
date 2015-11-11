95732fe826e55f2259ec8b695a8831190dd23135
David Boschwitz <hihid27@aim.com>
2015-11-11 14:57:02 -0600

    Added file

---
+++ myaccount.php
@@ -0,0 +1,17 @@
+<?php
+$page['auth'] = 1;
+require 'include/functions.inc';
+
+?>
+<html>
+    <head>
+        <title>Title</title>
+        <?php require 'include/head.inc'; ?>
+    </head>
+    <body>
+        <?php require 'include/header.inc'; ?>
+        
+        
+        <?php require 'include/footer.inc'; ?>
+    </body>
+</html>

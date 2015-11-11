dbad1c2dfb5926e2e4c1c8c9b48879ae15a2cbea
David Boschwitz <hihid27@aim.com>
2015-11-11 14:56:17 -0600

    Added file, usage for pointadmin.php actions applied to points

---
+++ pointadmin_functions.php
@@ -0,0 +1,22 @@
+<?php
+$page['auth'] = 7;
+include "include/functions.inc";
+
+switch (filter_input(INPUT_POST, 'task', FILTER_SANITIZE_STRING)) {
+    case 'edit':
+        //TODO
+        echo "Edit unsuccessful!";
+        break;
+    case 'delete':
+        //TODO
+        echo "Delete unsuccessful!";
+        break;
+    case 'editmultiple':
+        //TODO
+        echo "Edit unsuccessful!";
+        break;
+    case 'deletemultiple':
+        //TODO
+        echo "Delete unsuccessful!";
+        break;
+}

7fa85975a7e23a10451c55c3845e4d03ab07799d
David Boschwitz <hihid27@aim.com>
2015-11-15 13:01:50 -0600

    misc

--- test.php
+++ test.php
@@ -1,2 +1,13 @@
 <?php 
 
+include 'include/hash.php';
+
+$pass = "a nice password!";
+echo $pass."<br>";
+
+$hash1 = create_hash($pass);
+echo $hash1."<br>";
+
+$hash2 = create_hash($pass);
+echo $hash1."<br>";
+

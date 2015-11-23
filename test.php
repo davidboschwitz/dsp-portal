f5ce3a62de88259464b290cc9ff18318d1f93925
davidboschwitz <hihid27@aim.com>
2015-11-23 15:48:30 -0600

    misc

--- test.php
+++ test.php
@@ -1,13 +1,10 @@
-<?php 
-
-include 'include/hash.php';
-
-$pass = "a nice password!";
-echo $pass."<br>";
-
-$hash1 = create_hash($pass);
-echo $hash1."<br>";
-
-$hash2 = create_hash($pass);
-echo $hash1."<br>";
-
+<html>
+    <head>
+        <script>
+            <?php include 'flashing.js'; ?>
+        </script>
+    </head>
+    <body>
+        
+    </body>
+</html>

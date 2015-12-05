96e4f7a536b5de004ead9a954386493e71b25324
davidboschwitz <hihid27@aim.com>
2015-12-05 14:45:01 -0600

    changed mysqli back to mysql due to compatability issues

--- login.php
+++ login.php
@@ -9,16 +9,16 @@ $errormsg = "";
 //If a login attempt has been made
 if (filter_input(INPUT_POST, 'attempt', FILTER_SANITIZE_NUMBER_INT) > 0) {
     session_start();
-    include "include/mysql.inc";
-    
+    require "include/mysql.inc";
+
     $user = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);
     $pass = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_STRING);
-    
-    $query = sprintf("SELECT * FROM `dsp`.`dsp_users` WHERE `user` = '%s'", mysqli_real_escape_string($user));
-    $result = mysqli_query($query) or die('Invalid query: ' . mysqli_error());
-    $data = mysqli_fetch_assoc($result);
-    if ($data['user'] == $user) {
-        if ($data['pass'] == md5($pass)) {
+
+    $query = sprintf("SELECT * FROM `dsp`.`dsp_users` WHERE `user` = '%s'", mysql_escape_string($user));
+    $result = mysql_query($query) or die('Invalid query (A): ' . mysql_error());
+    $data = mysql_fetch_assoc($result);
+    if ($data['user'] === $user) {
+        if ($data['pass'] === md5($pass)) {
             $_SESSION['user'] = $user;
             $_SESSION['auth'] = $data['auth'];
             if ($data['auth'] >= 10)//webmaster
@@ -52,11 +52,12 @@ if (filter_input(INPUT_POST, 'attempt', FILTER_SANITIZE_NUMBER_INT) > 0) {
         <div id="login-box">
             <form id="login" name="login" method="POST" action="login.php">
                 <input id="attempt" name="attempt" type="hidden" value="<?php echo ((filter_input(INPUT_POST, 'attempt', FILTER_SANITIZE_NUMBER_INT)) + 1) . ""; ?>" />
-                <input id="user" name="user" type="text" placeholder="Username" value="<?php echo filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);; ?>"/><br>
+                <input id="user" name="user" type="text" placeholder="Username" value="<?php echo filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);?>"/><br>
                 <input id="pass" name="pass" type="password" placeholder="Password" /><br>
                 <input id="submit" type="submit" value="Login" />
             </form>
         </div>
     </body>
 </html>
-<?php mysqli_close($mysql_link);
+<?php
+mysql_close($mysql_link);

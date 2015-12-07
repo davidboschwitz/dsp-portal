61ce65667beaf03b36d957a64b5e191046915376
davidboschwitz <hihid27@aim.com>
2015-12-06 23:29:14 -0600

    added more bootstrap formatting

--- award.php
+++ award.php
@@ -45,15 +45,15 @@ session_start();
                     minlength: 2,
                     source: "./getusers.php?multiple"
                 });
-                
+
             });
-            
-            function submitaward(){
-                if(!document.getElementById("multiple").checked)
-                if($("#awardedto").val() === ""){
-                    $("#awardedto").class += " has-error";
-                    return false;
-                }
+
+            function submitaward() {
+                if (!document.getElementById("multiple").checked)
+                    if ($("#awardedto").val() === "") {
+                        $("#awardedto").class += " has-error";
+                        return false;
+                    }
             }
         </script>
     </head>
@@ -61,44 +61,46 @@ session_start();
         <?php require "include/header.inc"; ?>
         <form id="awardpts" action="submitaward.php" method="POST" on class="form-group">
             <h2>Award new points</h2>
-            <table class="award">
-                <tr class="form-inline">
-                    <td class="awardlabel">Code</td>
-                    <td><input id="code" name="code" type="text" maxlength="5" readonly required class="form-control " />&nbsp;[ <a onclick="openHints()" style="text-decoration: underline; color:blue;" href="#">list</a> ]</td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Quantity</td>
-                    <td><input id="quantity" name="quantity" type="number" min="1" max="20" value="1" required class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Award to</td>
-                    <td> <input id="awardedto" name="awardedto" type="text" placeholder="net-id to award to" required class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Award to multiple members?</td>
-                    <td><input id="multiple" name="multiple" type="checkbox" onchange="multipleCheck()" class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Award to multiple<br><i>(comma separate users)</i></td>
-                    <td><textarea id="awardedtomultiple" name="awardedtomultiple" type="text" placeholder="multiple net-ids (comma separated)" style="width: 390px; height:80px; margin:0px" class="form-control" ></textarea></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Comments</td>
-                    <td><input id="comments" name="comments" type="text" style="width: 100%;" class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Awarded by</td>
-                    <td><p class="form-control-static"><?php echo $_SESSION['user']; ?></p></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel">Awarded on</td>
-                    <td><p class="form-control-static"><?php echo date("Y-m-d h:m:s"); ?></p></td>
-                </tr>
-                <tr>
-                    <td class="awardlabel"></td>
-                    <td><input id="submit" type="submit" value="Submit" onclick="submitaward()" /></td>
-                </tr>
-            </table>
+            <div class="center-block" style="width: 50%">
+                <table class="table table-bordered table-striped table-hover table-condensed">
+                    <tr class="form-inline">
+                        <td class="awardlabel">Code</td>
+                        <td><input id="code" name="code" type="text" maxlength="5" readonly required class="form-control " />&nbsp;[ <a onclick="openHints()" style="text-decoration: underline; color:blue;" href="#">list</a> ]</td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Quantity</td>
+                        <td><input id="quantity" name="quantity" type="number" min="1" max="20" value="1" required class="form-control" /></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Award to</td>
+                        <td> <input id="awardedto" name="awardedto" type="text" placeholder="net-id to award to" required class="form-control" /></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Award to multiple members?</td>
+                        <td><input id="multiple" name="multiple" type="checkbox" onchange="multipleCheck()" class="form-control" /></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Award to multiple<br><i>(comma separate users)</i></td>
+                        <td><textarea id="awardedtomultiple" name="awardedtomultiple" type="text" placeholder="multiple net-ids (comma separated)" style="width: 390px; height:80px; margin:0px" class="form-control" ></textarea></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Comments</td>
+                        <td><input id="comments" name="comments" type="text" style="width: 100%;" class="form-control" /></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Awarded by</td>
+                        <td><p class="form-control-static"><?php echo $_SESSION['user']; ?></p></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel">Awarded on</td>
+                        <td><p class="form-control-static"><?php echo date("Y-m-d h:m:s"); ?></p></td>
+                    </tr>
+                    <tr>
+                        <td class="awardlabel"></td>
+                        <td><input id="submit" type="submit" value="Submit" onclick="submitaward()" /></td>
+                    </tr>
+                </table>
+            </div>
         </form>
         <?php require "include/footer.inc"; ?>
     </body>

5bf7df9bbbad69d5c5ad55659047a96e1efd1bc6
davidboschwitz <hihid27@aim.com>
2015-12-07 00:59:16 -0600

    updated bootstrap formatting

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if (filter_input(INPUT_POST, 'attempt', FILTER_SANITIZE_NUMBER_INT) > 0) {
         <?php include "include/head.inc"; ?>
     </head>
     <body>
-        <div class="container mycontent" style="height:100%"><div id="all-content" class="container mycontent">
+        <div class="container mycontent" style="height:100%">
                 <div id="sidebar">
                     <nav class="navbar navbar-default">
                         <div class="container-fluid">

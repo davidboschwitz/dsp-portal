021afe7ccb8cb0474d569374b426727736a35001
davidboschwitz <hihid27@aim.com>
2015-12-06 21:33:39 -0600

    added more bootstrap formatting

--- calendar.php
+++ calendar.php
@@ -10,7 +10,7 @@ require "include/functions.inc";
     <body>
         <?php require "include/header.inc"; ?>
         <h2>Calendar</h2>
-        <iframe src="<?php echo $config['calendar_iframe_src']; ?>" width="98%" height="96%"></iframe>
+        <iframe src="<?php echo $config['calendar_iframe_src']; ?>" height="96%"></iframe>
         <?php require "include/footer.inc"; ?>
     </body>
 </html>

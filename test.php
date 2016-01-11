ea5f84646f149557a28bc350b9d3587c0ff79d27
David Boschwitz <dbosch@iastate.edu>
2016-01-11 16:51:00 -0600

    Testing password hashing

--- test.php
+++ test.php
@@ -4,11 +4,19 @@ include 'include/hash.php';
 
 echo $a = create_hash("Hello 1");
 echo "<br>";
+echo str_len($a);
+echo "<br>";
+echo "<br>";
 
-echo create_hash("Hello 1");
+echo $b = create_hash("Hello 1");
+echo "<br>";
+echo str_len($b);
+echo "<br>";
 echo "<br>";
 
 echo validate_password("Hello 1", $a);
+echo "<br>";
+echo validate_password("Hello 1", $b);
 echo "<br>";
 
 echo validate_password("hello 1", $a);

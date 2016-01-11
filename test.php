f79b41e8cedad645668d93bb72d3f0abec21cb32
David Boschwitz <dbosch@iastate.edu>
2016-01-11 16:44:54 -0600

    Testing password hashing

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
-<html>
-    <head>
-        <script>
-            <?php include 'flashing.js'; ?>
-        </script>
-    </head>
-    <body>
-        
-    </body>
-</html>
+<?php
+
+include 'include/hash.php';
+
+echo $a = create_hash("Hello 1");
+echo "<br>";
+
+echo create_hash("Hello 1");
+echo "<br>";
+
+echo validate_password("Hello 1", $a);
+echo "<br>";

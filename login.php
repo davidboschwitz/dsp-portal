2fe6ef50fe539c8728af241b24b46064aef00ddf
David Boschwitz <dbosch@iastate.edu>
2016-01-11 19:36:33 -0600

    added num_login field to database login info

--- login.php
+++ login.php
@@ -36,6 +36,7 @@ if (filter_input(INPUT_POST, 'attempt', FILTER_SANITIZE_NUMBER_INT) > 0) {
     $data = mysql_fetch_assoc($result);
     if ($data['user'] === $user) {
         if (validate_password($pass, $data['pass'])) {
+            mysql_query("UPDATE `dsp_users` SET `num_login` = `num_login` + 1 WHERE `user` = "%s";",mysql_escape_string($user));
             $_SESSION['user'] = $user;
             $_SESSION['auth'] = $data['auth'];
             if ($data['auth'] >= 100)//webmaster

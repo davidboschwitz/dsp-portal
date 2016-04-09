3669b37c06c536312fde243b253bcf829e412ffa
David Boschwitz <dbosch@iastate.edu>
2016-04-08 20:38:07 -0500

    pretty alerts

--- pointadmin.php
+++ pointadmin.php
@@ -97,7 +97,7 @@ $rowcount = --$i; //Otherwise returns extra empty NULL row
             });
             function editPoint(pointID) {
                 $.post("pointadmin_functions.php", {task: "edit", pointid: pointID}, function (data) {
-                    swal(data);
+                    swal("", data, "info");
                 });
             }
             function deletePoint(pointID) {

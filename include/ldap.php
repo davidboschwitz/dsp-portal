463fbdc37772f9623e666a63140cf3035e1e720e
David Boschwitz <hihid27@aim.com>
2016-12-05 21:29:03 -0600

    Create ldap.php

---
+++ include/ldap.php
@@ -0,0 +1,38 @@
+  <?php
+  /*
+   * checks the credentials against the LDAP server
+   * $user - 
+   * $pass - password
+   */
+   function ldap_authenticate($user, $pass) {
+
+      // prevents guest account access
+      if($pass == "") {
+         return false;
+      }
+
+      try {
+         $Yldap_location = "ldaps://ldap.iastate.edu";
+         $ldap_port = 636;
+
+         // call the ldap connect function
+         $Ydatabase = ldap_connect($Yldap_location, $ldap_port);
+
+         // bind the connection
+         $good = @ldap_bind($Ydatabase, "uid=".$user.",ou=People,dc=iastate,dc=edu", $pass);
+
+         if($good) {
+            // valid credentials
+            return true;
+         } else {
+            // invalid credentials
+            return false;
+         }
+
+      } catch(Exception $e){
+        //var_dump($e);
+         return false;
+      }
+   }
+   ?>
+   
